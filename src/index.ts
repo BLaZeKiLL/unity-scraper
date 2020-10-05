f8b06f518bb11c22bb3b36cb8cb4da24513a7e4d
CodeBlaze <blazeingamer10@gmail.com>
2020-10-06 00:06:07 +0530

    puppeteer closed

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,36 @@ import { promises as fs } from 'fs';
 
 (async () => {
 
-  const archiveUrl = process.env.UNITY_ARCHIVE_URL || 'https://unity3d.com/get-unity/download/archive';
-  const jsonPath = process.env.UNITY_VERSIONS_PATH || 'unity-versions.json';
-
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+
+  try {
+    const archiveUrl = process.env.UNITY_ARCHIVE_URL || 'https://unity3d.com/get-unity/download/archive';
+    const jsonPath = process.env.UNITY_VERSIONS_PATH || __dirname + '/unity-versions.json';
   
-  await page.goto(archiveUrl);
-
-  const unityVersionInfo = await page.evaluate(
-    () => Array.from(
-      document.querySelectorAll('.unityhub') as NodeListOf<HTMLAnchorElement>,
-      a => {
-        const link = a.href.replace('unityhub://', '');
-        return {
-          version: link.split('/')[0],
-          changeset: link.split('/')[1]
+    const page = await browser.newPage();
+  
+    await page.goto(archiveUrl);
+  
+    const unityVersionInfo = await page.evaluate(
+      () => Array.from(
+        document.querySelectorAll('.unityhub') as NodeListOf<HTMLAnchorElement>,
+        a => {
+          const link = a.href.replace('unityhub://', '');
+          return {
+            version: link.split('/')[0],
+            changeset: link.split('/')[1]
+          }
         }
-      }
-    )
-  );
-
-  await fs.writeFile(jsonPath, JSON.stringify(unityVersionInfo), {encoding:'utf-8'});
+      )
+    );
+  
+    await fs.writeFile(jsonPath, JSON.stringify(unityVersionInfo), {encoding:'utf-8'});
+  
+    console.log('DONE');
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await browser.close();
+  }
 
 })();
